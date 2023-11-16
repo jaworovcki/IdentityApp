7dd06f5de96215b9a96d014ab6ee00f8c7ede159
Andrii <jaworovcki@gmail.com>
2023-11-16 18:12:07 +0200

    Configure IdentityCore + create migrations

---
+++ API/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace API.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,9 @@
+using API.Data;
+using API.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,12 +15,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//defining out IdentityCore service
+builder.Services.AddIdentityCore<User>(options =>
+{
+    //for password configuration
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+
+    //for email configuration
+    options.SignIn.RequireConfirmedEmail = true;
+})
+    .AddRoles<IdentityRole>() // to be able to add roles
+    .AddRoleManager<RoleManager<IdentityRole>>() // to be able to make use of Role Manager
+    .AddEntityFrameworkStores<DataContext>() // providing out context
+    .AddSignInManager<SignInManager<User>>() // make use of SignIn Manager
+    .AddUserManager<UserManager<User>>() // make use of User Manager to create users
+    .AddDefaultTokenProviders(); // to be able to crate tokens for email confirmation
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{ 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
