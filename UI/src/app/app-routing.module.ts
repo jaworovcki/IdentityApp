5a92faa549e0ae1114dcc6a618b371bbc2dbff99
Andrii <jaworovcki@gmail.com>
2023-11-18 12:50:03 +0200

    implement routing

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: "full", redirectTo: 'home'},
+  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
+  {path: 'home', component: HomeComponent},
+  {path: 'not-found', component: NotFoundComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
