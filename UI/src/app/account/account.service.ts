8f15680ffda9ffc370f6160d144e2091a98efbd2
Andrii <jaworovcki@gmail.com>
2023-11-19 14:46:31 +0200

    complete login component

--- UI/src/app/account/account.service.ts
+++ UI/src/app/account/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Register } from '../shared/models/register';
 import { environment } from 'src/environments/environment.development';
+import { Login } from '../shared/models/login';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class AccountService {
 
   register(model: Register) {
     return this.http.post(`${environment.apiUrl}account/register`, model);
+  }
+
+  login(model: Login) {
+    return this.http.post(`${environment.apiUrl}account/login`, model);
   }
 }
--- UI/src/app/account/login/login.component.ts
+++ UI/src/app/account/login/login.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+  submitted = false;
+  errorMessages: string[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+    private accountService: AccountService,
+    private router: Router) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      password : ['', [Validators.required]],
+    });
+  }
+
+  login() {
+    this.submitted = true;
+    this.errorMessages = [];
+
+    if (this.loginForm.valid) {
+      this.accountService.login(this.loginForm.value).subscribe({
+        next: (response: any) => {
+          localStorage.setItem('token', response.value.token);
+          this.router.navigateByUrl('/home');
+        },
+        error: (errorResponse) => {
+          console.log(errorResponse);
+        }
+      });
+    }
+  }
 
 }
